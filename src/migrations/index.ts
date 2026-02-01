72aafc8bcddf2d18200d694a6f4b054321cba628
human <skuranov09@gmail.com>
2026-02-01 23:17:01 +1200

    migrate

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,5 +1,6 @@
 import * as migration_20260127_065414_rename_my_collection from './20260127_065414_rename_my_collection';
 import * as migration_20260201_110249_init from './20260201_110249_init';
+import * as migration_20260201_111626_init from './20260201_111626_init';
 
 export const migrations = [
   {
@@ -10,6 +11,11 @@ export const migrations = [
   {
     up: migration_20260201_110249_init.up,
     down: migration_20260201_110249_init.down,
-    name: '20260201_110249_init'
+    name: '20260201_110249_init',
+  },
+  {
+    up: migration_20260201_111626_init.up,
+    down: migration_20260201_111626_init.down,
+    name: '20260201_111626_init'
   },
 ];
